13f8ea4a51c0822fc42d95cac3b5080c74af1ac5
Jonas Knarbakk <jonas@knarbakk.no>
2016-05-05 23:13:46 +0200

    Started work on quote button

--- lib/QuoteButton.jsx
+++ lib/QuoteButton.jsx
@@ -21,22 +21,6 @@ class QuoteButton extends React.Component {
     // Adding a `displayName` makes debugging React easier
     static displayName = 'QuoteButton';
 
-    // Test data
-    quotes = [
-        {
-            text: 'Jeg lukker mine øyne for å se',
-            author: 'Paul Gauguin'
-        },
-        {
-            text: 'Der alle tenker likt, blir det ikke tenkt mye.',
-            author: 'Walter Lippmann'
-        },
-        {
-            text: 'Alle har talent. Det uvanlige er å ha mot til å følge talentet til de mørke stedene den leder.',
-            author: 'Erica Jong'
-        },
-    ];
-
     // Since our button is being injected into the Composer Footer,
     // we receive the local id of the current draft as a `prop` (a read-only
     // property). Since our code depends on this prop, we mark it as a requirement.
@@ -44,17 +28,13 @@ class QuoteButton extends React.Component {
         draft: React.PropTypes.object.isRequired,
         session: React.PropTypes.object.isRequired,
     };
+    
+    var quoteHandler = new QuoteHandler();
 
     // We need to bind 'this' to each function we want to use "this" in
     constructor() {
         super();
-        this._getRandomQuote = this._getRandomQuote.bind( this );
         this._onQuoteRefresh = this._onQuoteRefresh.bind( this );
-        this._createQuoteTags = this._createQuoteTags.bind( this );
-        this._getEmailContent = this._getEmailContent.bind( this );
-        this._insertQuotesToTags = this._insertQuotesToTags.bind( this );
-        this._replaceEmailContent = this._replaceEmailContent.bind( this );
-        this._formatQuoteObjectToString = this._formatQuoteObjectToString.bind( this );
 
         // Search for [ quote ] and add tags with qoute
     }
@@ -66,39 +46,11 @@ class QuoteButton extends React.Component {
         return nextProps.session !== this.props.session;
     }
 
-    // Refresh quote
-    _onQuoteRefresh() {
-        console.log( 'Refresh quote' );
-
-        // Get tags in body
-        var content = this._getEmailContent();
-
-        // Change [ quote ] to <span class="random-quote"></span>
-        content = this._createQuoteTags( content );
-
-        // Insert quote to each random quote tag
-        content = this._insertQuotesToTags( content );
-
-        // Save the changes
-        content = this._replaceEmailContent( content );
-
-        console.log(content);
-
-    }
-
-    // Create quote tag
-    // [ quote ] to <span class="random-quote"></span>
-    _createQuoteTags( content ) {
-        return content.replace( /\[ *quote *\]/g, '<span class="random-quote"></span>' );
-    }
-
-    // Replace mail
-
     // Get content of email
     _getEmailContent() {      
         return this.props.draft.body;
     }
-
+   
     // Replace email body
     _replaceEmailContent( newContent ) {
         // The new text of the draft is our translated response, plus any quoted text
@@ -113,46 +65,11 @@ class QuoteButton extends React.Component {
 
         // Return the new content
         return newContent;
-    }
+    }}
 
-    // Format the quote object to quote string
-    _formatQuoteObjectToString( quoteObject ) {
-        return quoteObject.text + ' - ' + quoteObject.author;
-    }
-
-    // Search for all span.random-quote and replace its content with new quote data
-    _insertQuotesToTags( content ) {
-
-        // Find each span.class element
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(content, "text/html");
-        var spanElements = doc.querySelectorAll('span.random-quote');
-   
-        // Loop each
-        for (var i = 0, len = spanElements.length; i < len; i++) {
-            var spanElement = spanElements[ i ];
-            var quoteString = this._formatQuoteObjectToString( this._getRandomQuote() );
-
-            // Insert quote to tag
-            spanElement.innerHTML = quoteString;
-            // console.log(quoteString);
-        }
-
-        // Convert domParser back to string
-        // This is the hard way of doing this --- FIX THIS ---
-        content = content.replace( /<body>.+<\/body>/, '<body>'+doc.body.innerHTML+'</body>' );
-
-        // Remove this. ( Only for debugging )
-        window.$doc = doc;
-
-        // return new content
-        return content;
-
-    }
-
-    // Get random quote
-    _getRandomQuote() {
-        return this.quotes[Math.floor(Math.random()*this.quotes.length)];
+    // Refresh quote
+    _onQuoteRefresh() {
+        console.log('clicky, clicky');
     }
 
     render() {
