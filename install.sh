e2c22a209ff3e9cc1897ecc7181a470ffffba310
Lasse S. Haslev <lasse@haslev.no>
2016-05-05 18:56:01 +0200

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set the path to this plugin
-nylaspath='~/.nylas/dev/packages/'
+nylaspath=~/.nylas/dev/packages/random-quotes
 
 # Clone repository to the development folder
 git clone https://github.com/LasseHaslev/N1-quotes.git $nylaspath;
 
 # cd into repository
-cd $nylaspath > 'random-signature-quotes';
+cd $nylaspath;
 
 # Install node
 npm install
